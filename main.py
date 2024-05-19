81d18bfc96ec39b3de884a2774e57da1e3db1f92
mxblsdl <maxblasdel@gmail.com>
2024-05-18 17:37:24 -0700

    support file download

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI, UploadFile, File, HTTPException, Depends
+from fastapi import FastAPI, UploadFile, File, HTTPException, Depends, Response
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
 import aiosqlite
@@ -42,6 +42,7 @@ app.add_middleware(
 
 async def get_db():
     db = await aiosqlite.connect("file_uploads.db")
+    await db.set_trace_callback(print)
     try:
         yield db
     finally:
@@ -72,6 +73,21 @@ async def get_files(db: aiosqlite.Connection = Depends(get_db)):
     rows = await cursor.fetchall()
     filenames = [{"filename": row[0]} for row in rows]
     return {"filenames": filenames}
+
+
+@app.get("/files/{file_id}")
+async def get_file(file_id: str, db: aiosqlite.Connection = Depends(get_db)):
+    cursor = await db.execute("SELECT id, bin FROM data WHERE id = ?", (file_id,))
+    row = await cursor.fetchone()
+    print(row)
+    if row is None:
+        raise HTTPException(status_code=404, detail="File not found")
+    filename, content = row
+    return Response(
+        content=content,
+        media_type="application/octet-stream",
+        headers={"Content-Disposition": f"attachment; filename={filename}"},
+    )
 
 
 @app.get("/")
