826433cd46285879cf4992bdd31c422291e970e2
mxblsdl <maxblasdel@gmail.com>
2024-05-22 20:33:27 -0700

    add upload util

---
+++ utils.py
@@ -0,0 +1,16 @@
+import requests
+
+# Code to interact with API from a python script
+
+
+def upload_file(path: str) -> None:
+    file = {"file": (path, open(path, "rb"))}
+    response = requests.post("http://127.0.0.1:8000/upload/", files=file)
+    return response.json()
+
+
+def list_files() -> list[str]:
+    res = requests.get("http://127.0.0.1:8000/files/")
+    if res.ok:
+        filenames = res.json()
+        return [f["filename"] for f in filenames["filenames"]]
